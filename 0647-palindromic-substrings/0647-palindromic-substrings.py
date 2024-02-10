c76c36892553fdb14f68ce6d7905b763c721278e
harshitabhambhani <109619297+harshitabhambhani@users.noreply.github.com>
2024-02-10 18:59:43 +0530

    Time: 151 ms (35.03%), Space: 19.4 MB (22.88%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        count = 0
+        
+        # Create a boolean 2D array to store whether a substring is a palindrome
+        dp = [[False] * n for _ in range(n)]
+        
+        # A single character is always a palindrome
+        for i in range(n):
+            dp[i][i] = True
+            count += 1
+        
+        # Check for palindromes of length 2
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                count += 1
+        
+        # Check for palindromes of length greater than 2
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    count += 1
+        
+        return count
+
+        
